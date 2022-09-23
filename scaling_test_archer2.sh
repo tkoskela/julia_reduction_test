d3a05725ee02c5c2543919b95d571313f1d3faa3
Tuomas Koskela <t.koskela@ucl.ac.uk>
2022-09-23 15:57:44 +0100

    modify job script to run multiple numbers of tasks

--- scaling_test_archer2.sh
+++ scaling_test_archer2.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 #SBATCH --job-name=julia_reduction_test
-#SBATCH --time=0:10:0
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=32
+#SBATCH --time=00:20:00
+#SBATCH --nodes=16
+#SBATCH --tasks-per-node=64
 #SBATCH --cpus-per-task=1
 
-# Replace [budget code] below with your budget code (e.g. t01)
-#SBATCH --account=e723
+#SBATCH --account=ta084-tk-driving
 #SBATCH --partition=standard
-#SBATCH --qos=short
+#SBATCH --qos=lowpriority
 
-# Set the number of threads to 1
-#   This prevents any threaded system libraries from automatically 
-#   using threading.
 export OMP_NUM_THREADS=1
 
-export JULIA_DEPOT_PATH=/work/e723/e723/tkoskela/julia_depot
-export julia_exe=/work/e723/e723/tkoskela/julia_install/julia-1.8.1/bin/julia
-export julia_script=/work/e723/e723/tkoskela/julia_reduction_test/reduction.jl
-srun --nodes=1 --ntasks=32 --distribution=block:block --hint=nomultithread $julia_exe $julia_script
+export WRKDIR=/work/ta084/ta084/tk-drivingtest
+
+export JULIA_DEPOT_PATH=$WRKDIR/julia-depot
+export julia_exe=$WRKDIR/julia-1.8.1/bin/julia
+export julia_script=$WRKDIR/julia_reduction_test/reduction.jl
+for tasks in 16 32 64 128 256 512 1024
+do
+    srun --nodes=16 --ntasks=$tasks --distribution=block:block --hint=nomultithread $julia_exe $julia_script
+done
